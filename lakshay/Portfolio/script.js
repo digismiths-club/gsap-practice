41221735e3fb7dbaa0ddb56c32aee805eea5cdeb
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-04-11 00:24:50 +0530

    Animations added

--- lakshay/Portfolio/script.js
+++ lakshay/Portfolio/script.js
@@ -1,6 +1,8 @@
 // gsap.registerPlugin(ScrollSmoother);
 
 
+// Hero animation
+
 gsap.fromTo(
     ".hero-img",
     {
@@ -20,3 +22,50 @@ gsap.fromTo(
 //     content:"body",
 //     smooth: 2
 // })
+
+
+
+gsap.to(
+    ".marquee",
+    {
+        xPercent: -30,
+        repeat: -1,
+        duration: 6,
+        ease: "linear"
+    }
+).totalProgress(0.5)
+
+gsap.set(
+    ".marque",
+    {
+        xPercent: -50
+    }
+)
+
+
+// socials animation
+
+let socials= [document.getElementById("social1"),document.getElementById("social2"), document.getElementById("social3"), document.getElementById("social4")];
+
+
+for(let i=0;i<4;i++){
+    socials[i].addEventListener("mouseenter",(e)=>{
+        gsap.to(
+            e.target,
+            {
+                y: -15,
+                ease:"ease-in",
+                duration: 0.5
+            }
+        )
+    })
+    socials[i].addEventListener("mouseleave",(e)=>{
+        gsap.to(
+            e.target,
+            {
+                y: 0,
+                ease:"elastic",
+            }
+        )
+    })
+}
