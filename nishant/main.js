85411b0f03e06fb1169b9b7d80fb568e636a6ae7
Nishant Chhillar <nchhillar2004@gmail.com>
2024-04-12 16:22:39 +0530

    final commit

--- nishant/main.js
+++ nishant/main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './css/style.css';
 import gsap from 'gsap';
 
-gsap.to(".app", {x:100, y: -100, duration: 2})
+gsap.to("#content", {y: 100, duration: 2, repeat: -1, yoyo: true})
